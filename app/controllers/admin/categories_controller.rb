c8ca94de757a630729ff3d17f21c2d34cf8846e9
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-12 19:07:15 -0400

    Admin::CategoriesController with all relevant methods defined

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,38 @@
+class Admin::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to(@category)
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
+end
