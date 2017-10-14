cad96fb09b3a2cc975537bc86c3098188bdcbf01
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-14 16:33:13 -0400

    validate rating data before database insertion

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,21 @@
 class Review < ActiveRecord::Base
   belongs_to :product
+
+  validates :product_id,
+            presence: true
+  validates :user_id,
+            presence: true
+  validates :description,
+            presence: true,
+            length: { minimum: 10 }
+  validates :rating,
+            presence: true
+  validate :valid_rating
+
+  def valid_rating
+    unless self.rating >= 1 && self.rating <= 10
+      errors.add(:rating, "Rating must be between 1 and 10")
+    end
+  end
+
 end
