310181ee8e84a55858d4b5078583aaa3b7dd3afd
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-14 18:36:42 -0400

    show reviews when rendering producs#show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,8 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
+    @review = Review.new
+    @user_reviews = Review.where(user_id: current_user)
   end
 
 end
