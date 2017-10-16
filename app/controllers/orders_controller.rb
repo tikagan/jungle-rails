0e4436990807a88e1cecbf1cfdc9c96761b7b30d
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-16 13:06:29 -0400

    update Stripe::Charge params to show current user's info

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -39,7 +39,7 @@ class OrdersController < ApplicationController
     Stripe::Charge.create(
       source:      params[:stripeToken],
       amount:      cart_total, # in cents
-      description: "Khurram Virani's Jungle Order'"
+      description: params[:description],
       currency:    'cad'
     )
   end
