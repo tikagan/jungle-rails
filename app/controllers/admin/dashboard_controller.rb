a7f9e9b49053948b8c05cd70d3ff12b33b495816
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-16 17:26:22 -0400

    inherit http authentication from Admin::BaseController

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,6 @@
-class Admin::DashboardController < ApplicationController
-
-  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+class Admin::DashboardController < Admin::BaseController
 
   def show
   end
+
 end
